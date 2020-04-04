841f38e2717420fafbcd51408d45aed0489a25e6
Vishal Varghese <vishalvarghese46@gmail.com>
2020-04-05 00:15:05 +0100

    working with multiprocessiong

--- multiprocessLearn/multi.py
+++ multiprocessLearn/multi.py
@@ -1,8 +1,11 @@
 from collections import namedtuple
 from pprint import PrettyPrinter
 import json
+import time
+import multiprocessing
 
 pp = PrettyPrinter(indent=4)
+
 
 Store = namedtuple('Store', "id name elecMeter gasMeter")
 data = json.load(open(r"F:\Python\Selenium\func\meterRef.json"))
@@ -22,13 +25,22 @@ for i in x:
 
 
 database = tuple(database)
-pp.pprint(database)
+#pp.pprint(database)
 print("\n")
 
 g = filter(lambda x: x.gasMeter is not None, database)
-pp.pprint(tuple(g))
+#pp.pprint(tuple(g))
 
 
+start = time.time()
 
+pool = multiprocessing.Pool()
 
+calc = pool.map(lambda x: { 'difference': x.gasMeter-x.elecMeter,
+                       'name': 'kundi'},g)
+time.sleep(1)
+end = time.time()
 
+for i in calc:
+    pp.pprint(i)
+print(f"Task done in: {end-start:.2f}s")
